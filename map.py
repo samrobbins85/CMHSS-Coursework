452fd737c87f12f1b81ecbafa88970c103b01877
Sam Robbins <samrobbinsgb@gmail.com>
2021-04-16 17:08:57 +0100

    Add geojson

--- map.py
+++ map.py
@@ -2,13 +2,13 @@ import pandas as pd
 import json
 from urllib.request import urlopen
 import numpy as np
-
+# Code sourced from https://focaalvarez.medium.com/mapping-the-uk-and-navigating-the-post-code-maze-4898e758b82f
 #Load GeoJson 
-with urlopen('https://opendata.arcgis.com/datasets/48b6b85bb7ea43699ee85f4ecd12fd36_4.geojson') as response:
+with open('nuts1.geojson') as response:
     counties = json.load(response)
  
 #Load data to be charted    
-dummy_data=pd.read_csv('https://opendata.arcgis.com/datasets/48b6b85bb7ea43699ee85f4ecd12fd36_0.csv?outSR=%7B%22latestWkid%22%3A27700%2C%22wkid%22%3A27700%7D')
+dummy_data=pd.read_csv('nuts1.csv')
 #add dummy data
 dummy_data['value']=np.random.randint(10, 100, size=len(dummy_data))
 
@@ -19,9 +19,9 @@ from geojson_rewind import rewind
 #Make the rings clockwwise (to make it compatible with plotly)    
 counties_corrected=rewind(counties,rfc7946=False)
 
-fig = px.choropleth(dummy_data, geojson=counties_corrected, locations='nuts218cd', featureidkey="properties.nuts218cd", color='value',
+fig = px.choropleth(dummy_data, geojson=counties_corrected, locations='nuts118cd', featureidkey="properties.nuts118cd", color='value',
                             color_continuous_scale="PurPor", labels={'label name':'label name'}, title='MAP TITLE',
                             scope="europe")
 
 fig.update_geos(fitbounds="locations", visible=False)
-fig.show()
+fig.write_image("fig1.png")
