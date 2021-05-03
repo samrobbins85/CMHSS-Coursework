33ecfbbbbefe3be39d58e7f8004e8c57bcc2bb6d
Sam Robbins <samrobbinsgb@gmail.com>
2021-05-03 15:57:17 +0100

    Add images

--- map.py
+++ map.py
@@ -8,10 +8,15 @@ with open('new.geojson') as response:
     counties = json.load(response)
 print("Geojson imported")
 #Load data to be charted    
-dummy_data=pd.read_csv('nuts1.csv')
+dummy_data=pd.read_csv("nuts1.csv")
+print(dummy_data)
+real_data=pd.read_json('feature_stats.json')
+entries = real_data.columns
+real_data.index.name = 'nuts118cd'
+real_data.reset_index(inplace=True)
+print(real_data)
 print("CSV Imported")
 #add dummy data
-dummy_data['value']=np.random.randint(10, 100, size=len(dummy_data))
 print("Dummy data generated")
 #With Plotly
 import plotly.express as px
@@ -21,12 +26,13 @@ from geojson_rewind import rewind
 counties_corrected=rewind(counties,rfc7946=False)
 print("Rewinding done")
 
-fig = px.choropleth(dummy_data, geojson=counties_corrected, locations='nuts118cd', featureidkey="properties.nuts118cd", color='value',
-                            color_continuous_scale="PurPor", labels={'label name':'label name'}, title='MAP TITLE',
-                            scope="europe")
-print("Figure generated")
-fig.update_geos(fitbounds="locations", visible=False)
-print("Geos updated")
-fig.write_image("fig1.png")
-print("Done")
+for item in entries:
+    fig = px.choropleth(real_data, geojson=counties_corrected, locations='nuts118cd', featureidkey="properties.nuts118cd", color=item,
+                                color_continuous_scale="PurPor", labels={'label name':'label name'}, title=item.capitalize(),
+                                scope="europe")
+    print("Figure generated")
+    fig.update_geos(fitbounds="locations", visible=False)
+    print("Geos updated")
+    fig.write_image("images/"+item+".png")
+    print("Done")
 # fig.show()
